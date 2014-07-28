c6d9ab48cce88cb2071436641e6af38bf7505f17
Sweetheart502 <869780307@qq.com>
2014-07-28 11:02:22 +0800

    07/28 -- 11:02

--- group/interger_three.c
+++ group/interger_three.c
@@ -1,8 +1,20 @@
-/**
- *习题训练一
- *李映霄
+/*
+ * =====================================================================================
+ *
+ *       Filename:  interger_three.c
+ *
+ *    Description:  计算三加根号下五的N次方,输出后三位的结果。
+ *
+ *        Version:  1.0
+ *        Created:  2014年06月8日 10时30分48秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Sweetheart502, liyingxiao502@gmail.com
+ *        Company:  Class 1304 of Software Engineering
+ *
+ * =====================================================================================
  */
-
 
 #include <stdio.h>
 #include <math.h>
@@ -10,34 +22,34 @@
 
 int main(void)
 {
-	int a[N];
-	int number, i;
-	float result = 1;
+	int 	a[N];
+	int 	number, i;
+	float 	result = 1;
 
-	printf("input the number:\n");
-	scanf("%d", &number);
+	printf ( "input the number:\n" );
+	scanf ( "%d", &number );
 
 	//计算表达式结果
-	for (i = 0; i < number; i++) {
-		result *= 3 + sqrt(5);
+	for ( i = 0; i < number; i++ ) {
+		result *= 3 + sqrt ( 5 );
 	}
 
 	//输出结果以验证自己的结果
-	printf("%f\n", result);
+	printf ( "%f\n", result );
 	
 	//存储后三位在数组a中
-	for (i = 2 ; i >= 0; i--) {
-		a[i] = ((int)result) % 10;
+	for ( i = 2 ; i >= 0; i-- ) {
+		a[i] = ( ( int ) result ) % 10;
 		result /= 10;
 	}
 
 	//输出目标结果
-	printf("the interger is :\n");
-	for (i = 0; i < N; i++) {
-		printf("%d", a[i]);
+	printf ( "the interger is :\n" );
+	for ( i = 0; i < N; i++ ) {
+		printf ( "%d", a[i] );
 	}
 
-	printf("\n");
+	printf ( "\n" );
 
 	return 0;
 }
